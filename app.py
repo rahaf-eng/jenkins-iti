74e95d120056f84dc19dc354d5a57b0d85c536eb
Rahaf Mohamed <113079275+rahaf-eng@users.noreply.github.com>
2026-05-15 14:59:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
 def process_data():
-    print("Processing Data Engineering Task...")
+    print("Processing Data Engineering Task... Success!")
     return True
 
+@app.route('/')
+def hello_world():
+    # تصفح
+    return '<h1>Hello CI/CD from Rahaf 🚀</h1>'
+
 if __name__ == "__main__":
+
     process_data()
+    
+
+    app.run(host='0.0.0.0', port=5000)
